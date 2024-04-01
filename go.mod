bb7b9a8f42f560dd87b2efe12ef60d4f90df3079
Adrien Driffaud <adrien@driffaud.fr>
2024-04-01 17:55:08 +0200

    add templ

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adriffaud/todo_app
 
 go 1.21.8
+
+require github.com/a-h/templ v0.2.648 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/adriffaud/todo_app/components"
 )
 
+func index(w http.ResponseWriter, r *http.Request) {
+	components.Main().Render(r.Context(), w)
+}
+
 func main() {
-	fmt.Println("Hello world")
+	router := http.NewServeMux()
+	router.HandleFunc("/", index)
 
-	http.Handle("/")
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(server.ListenAndServe())
 }
